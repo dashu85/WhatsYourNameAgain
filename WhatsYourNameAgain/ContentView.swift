5a9df129bb4c2848239d4f5523da6d25fae9a6bb
Marcus Benoit <benoit.marcus@gmail.com>
2024-07-28 12:16:13 +0200

    Initial Commit

---
+++ WhatsYourNameAgain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WhatsYourNameAgain
+//
+//  Created by Marcus Benoit on 28.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WhatsYourNameAgain/WhatsYourNameAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsYourNameAgainApp.swift
+//  WhatsYourNameAgain
+//
+//  Created by Marcus Benoit on 28.07.24.
+//
+
+import SwiftUI
+
+@main
+struct WhatsYourNameAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
